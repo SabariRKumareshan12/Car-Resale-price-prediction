b789aff333de9f42316e84c67e91a447b9b553f7
Sabari Raghul Kumareshan <raghul12ksr@gmail.com>
2025-06-18 14:15:24 +0530

    Update Car_resale.py

--- Car_resale.py
+++ Car_resale.py
@@ -170,7 +170,7 @@ with tab3:
     st.header("Car Chatbot Assistant 💬")
     df = load_car_data()
             
-    user_query = st.text_input("Ask me about cars!", "Tell me about...")
+    user_query = st.text_input("Ask me about cars!", "Tell me about Tata Punch")
 
     if user_query:
             if "tell me about" in user_query.lower():
